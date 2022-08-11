4dc124fb5f627322f0ae20000a3016649af80b53
Batuhan Akbaş <batuhanakbas011@gmail.com>
2022-08-11 17:59:40 +0300

    component scans added

--- src/main/java/tech/reaven/application/StockApplication.java
+++ src/main/java/tech/reaven/application/StockApplication.java
@@ -1,17 +1,21 @@
-package tech.reaven;
+package tech.reaven.application;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import tech.reaven.StockListInitializer;
 import tech.reaven.api.StockController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import tech.reaven.model.Stock;
+import tech.reaven.repository.StockRepository;
 import tech.reaven.service.StockService;
 
 import java.util.List;
 
+@EnableJpaRepositories(basePackages = {"tech.reaven.repository"})
 @EntityScan("tech.reaven.model")
 @SpringBootApplication(scanBasePackages = {
         "tech.reaven.config",
@@ -19,7 +23,11 @@ import java.util.List;
         "tech.reaven.repository",
         "tech.reaven.api"
 })
-@ComponentScan(basePackageClasses = StockController.class)
+@ComponentScan(basePackageClasses = {
+        StockController.class,
+        StockService.class,
+        StockRepository.class
+})
 public class StockApplication {
     private List<Stock> stockList;
 
@@ -31,6 +39,6 @@ public class StockApplication {
     CommandLineRunner commandLineRunner(StockService stockService){
         StockListInitializer stockListInitializer = new StockListInitializer();
         stockList = stockListInitializer.InitializeStockList();
-        return args -> stockService.save(stockList.get(0));
+        return args -> stockService.updateAllStocks(stockList);
     }
 }
