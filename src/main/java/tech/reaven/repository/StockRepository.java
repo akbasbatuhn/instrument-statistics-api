b32248557657080b4e714108d1f8ebb69f66d2eb
Batuhan Akbaş <batuhanakbas011@gmail.com>
2022-08-12 13:25:05 +0300

    new: find() query

--- src/main/java/tech/reaven/repository/StockRepository.java
+++ src/main/java/tech/reaven/repository/StockRepository.java
@@ -1,9 +1,13 @@
 package tech.reaven.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tech.reaven.model.Stock;
 
 @Repository
-public interface StockRepository extends JpaRepository<Stock, Long> {
+public interface StockRepository extends JpaRepository<Stock, String> {
+    @Query(value = "SELECT * FROM stocks WHERE isincode LIKE BINARY CONCAT('%',:isincode,'%')", nativeQuery = true)
+    Stock findByISINCode(@Param("isincode") String stock_isin);
 }
