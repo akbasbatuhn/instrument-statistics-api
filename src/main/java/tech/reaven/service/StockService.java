0e6b35fb30a4aa9840f296548f6d5f80a329fe0c
Batuhan Akbaş <batuhanakbas011@gmail.com>
2022-08-16 09:29:33 +0300

    remove: saving stock from json file

--- src/main/java/tech/reaven/service/StockService.java
+++ src/main/java/tech/reaven/service/StockService.java
@@ -8,39 +8,12 @@ import org.springframework.web.server.ResponseStatusException;
 import tech.reaven.model.Stock;
 import tech.reaven.repository.StockRepository;
 
-import java.util.List;
-
 @Service
 @AllArgsConstructor
 public class StockService {
     @Autowired
     private StockRepository stockRepository;
 
-    public List<Stock> updateAllStocks(List<Stock> stockList){
-        List<Stock> tempList = stockRepository.findAll();
-        if(tempList.size() == 0){
-            save(stockList);
-        }
-        else{
-            for(Stock stock : tempList){
-                for (Stock tempStock : stockList){
-                    if (!stock.getName().equals(tempStock.getName())){
-                        save(stock);
-                    }
-                }
-            }
-        }
-        return tempList;
-    }
-
-    public Stock save(Stock stock){
-
-        return stockRepository.save(stock);
-    }
-
-    public Iterable<Stock> save(List<Stock> stockList){
-        return stockRepository.saveAll(stockList);
-    }
 
     public Stock find(String isinCode){
         Stock foundStock = stockRepository.findByISINCode(isinCode);
