c3803859dc4b62fe5c8e1e5898d0193f64958631
Batuhan Akbaş <batuhanakbas011@gmail.com>
2022-08-29 15:49:22 +0300

    refactor: application package scans comments disabled

--- src/main/java/tech/reaven/application/StockApplication.java
+++ src/main/java/tech/reaven/application/StockApplication.java
@@ -1,44 +1,32 @@
-package tech.reaven;
+package tech.reaven.application;
 
-import org.springframework.boot.CommandLineRunner;
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import tech.reaven.api.StockController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import tech.reaven.model.Stock;
+import tech.reaven.api.StockController;
+import tech.reaven.config.SwaggerConfig;
 import tech.reaven.repository.StockRepository;
 import tech.reaven.service.StockService;
 
-import java.util.List;
+import java.util.Arrays;
 
-//@EnableJpaRepositories(basePackages = {"tech.reaven.repository"})
-//@EntityScan("tech.reaven.model")
-//@SpringBootApplication(scanBasePackages = {
-//        "tech.reaven.config",
-//        "tech.reaven.service",
-//        "tech.reaven.repository",
-//        "tech.reaven.api"
-//})
-//@ComponentScan(basePackageClasses = {
-//        StockController.class,
-//        StockService.class,
-//        StockRepository.class
-//})
+@EnableJpaRepositories(basePackages = {"tech.reaven.repository"})
+@EntityScan("tech.reaven.model")
+@ComponentScan(basePackageClasses = {
+        StockController.class,
+        StockService.class,
+        StockRepository.class,
+        SwaggerConfig.class
+})
 @SpringBootApplication
 public class StockApplication {
-    private List<Stock> stockList;
 
     public static void main(String[] args) {
         SpringApplication.run(StockApplication.class);
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner(StockService stockService){
-        StockListInitializer stockListInitializer = new StockListInitializer();
-        stockList = stockListInitializer.InitializeStockList();
-        return args -> stockService.updateAllStocks(stockList);
-    }
 }
