d14ccc549976914fda9c58b402f1fd483fbac5fe
Batuhan Akbaş <batuhanakbas011@gmail.com>
2022-08-29 15:50:57 +0300

    feat: cors controller added

--- src/main/java/tech/reaven/api/StockController.java
+++ src/main/java/tech/reaven/api/StockController.java
@@ -1,47 +1,65 @@
 package tech.reaven.api;
 import org.springframework.beans.factory.annotation.Autowired;
-import tech.reaven.StockListInitializer;
+import org.springframework.http.HttpStatus;
+import tech.reaven.model.BaseData;
+import tech.reaven.model.FundamentalData;
+import tech.reaven.model.PriceInfo;
 import tech.reaven.model.Stock;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import tech.reaven.service.StockService;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
-@RestController
 @Tag(name = "Stock", description = "Stock Operations")
 @RequestMapping("/stock")
+@RestController
 public class StockController {
-    private List<Stock> stockList = new ArrayList<Stock>();
-
     @Autowired
     private StockService stockService;
 
-
-    @PostConstruct
-    private void init(){
-        StockListInitializer stockListInitializer = new StockListInitializer();
-        stockList = stockListInitializer.InitializeStockList();
-    }
-
-    @GetMapping("/{stockISIN}")
+    @GetMapping("/{isinCode}")
     @Operation(summary = "Get stock by ISIN Code", responses = {
             @ApiResponse(description = "Success", responseCode = "200",
-            content = @Content(mediaType = "application/json",
-            schema = @Schema(implementation = Stock.class))),
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Stock.class))),
             @ApiResponse(description = "Stock not found", responseCode = "404",
-            content = @Content)
+                    content = @Content)
     })
-    public ResponseEntity<Stock> getStock(String isinCode){
+    public ResponseEntity<Stock> getStock(@PathVariable String isinCode){
         return ResponseEntity.ok(stockService.find(isinCode));
     }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/all")
+    public ResponseEntity<List<Stock>> getAllStocks(){
+        List<Stock> stocks = stockService.findAllStocks();
+        return new ResponseEntity<>(stocks, HttpStatus.OK);
+    }
+
+//    @GetMapping("/{isinCode}")
+//    @Operation(summary = "Get stock by ISIN Code", responses = {
+//            @ApiResponse(description = "Success", responseCode = "200",
+//            content = @Content(mediaType = "application/json",
+//            schema = @Schema(implementation = Stock.class))),
+//            @ApiResponse(description = "Stock not found", responseCode = "404",
+//            content = @Content)
+//    })
+//    public ResponseEntity<Stock> getStock(@PathVariable String isinCode){
+//        return ResponseEntity.ok(stockService.find(isinCode));
+//    }
+//
+//    @CrossOrigin
+//    @RequestMapping(method = RequestMethod.GET, path = "/{isinCode}")
+//    public ResponseEntity<List<Stock>> getAllStocks(@PathVariable String isinCode){
+//        return new Stock("DE1234567890", "New stock",
+//                new PriceInfo(), new BaseData(), new FundamentalData(),
+//                "DENEME!'^^%&/()=");
+//        return ResponseEntity.ok(HttpStatus.OK);
+//    }
 }
