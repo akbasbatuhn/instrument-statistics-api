70b57e07cba262d521d670e42d9892981387c478
Batuhan Akbaş <batuhanakbas011@gmail.com>
2022-09-01 17:01:38 +0300

    refactor: cross origin annotation added and unnecessary comments deleted

--- src/main/java/tech/reaven/api/StockController.java
+++ src/main/java/tech/reaven/api/StockController.java
@@ -23,6 +23,7 @@ public class StockController {
     @Autowired
     private StockService stockService;
 
+    @CrossOrigin
     @GetMapping("/{isinCode}")
     @Operation(summary = "Get stock by ISIN Code", responses = {
             @ApiResponse(description = "Success", responseCode = "200",
@@ -41,25 +42,5 @@ public class StockController {
         List<Stock> stocks = stockService.findAllStocks();
         return new ResponseEntity<>(stocks, HttpStatus.OK);
     }
-
-//    @GetMapping("/{isinCode}")
-//    @Operation(summary = "Get stock by ISIN Code", responses = {
-//            @ApiResponse(description = "Success", responseCode = "200",
-//            content = @Content(mediaType = "application/json",
-//            schema = @Schema(implementation = Stock.class))),
-//            @ApiResponse(description = "Stock not found", responseCode = "404",
-//            content = @Content)
-//    })
-//    public ResponseEntity<Stock> getStock(@PathVariable String isinCode){
-//        return ResponseEntity.ok(stockService.find(isinCode));
-//    }
-//
-//    @CrossOrigin
-//    @RequestMapping(method = RequestMethod.GET, path = "/{isinCode}")
-//    public ResponseEntity<List<Stock>> getAllStocks(@PathVariable String isinCode){
-//        return new Stock("DE1234567890", "New stock",
-//                new PriceInfo(), new BaseData(), new FundamentalData(),
-//                "DENEME!'^^%&/()=");
-//        return ResponseEntity.ok(HttpStatus.OK);
-//    }
+    
 }
